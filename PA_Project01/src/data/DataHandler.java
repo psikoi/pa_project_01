78d46e0636e44e3bc4b569cae0e6a7a590501d63
Ruben <Ruben@192.168.1.6>
2018-01-06 17:22:50 +0000

    Adicionei o codigo de insertPlayer.

--- PA_Project01/src/data/DataHandler.java
+++ PA_Project01/src/data/DataHandler.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
 import data.dao.json.UserDAOJSON;
 import data.dao.json.MachineDAOJSON;
 import game.models.ComputerGame;
 import game.models.Game;
+import game.models.Machine;
 import game.models.Player;
 import game.models.User;
 
@@ -17,15 +13,21 @@ import game.models.User;
  * @author Tiago
  */
 public class DataHandler {
-    
-    public static void insertPlayer(Player player){
-        
+
+    public static void insertPlayer(Player player) {
+        if (player instanceof User) {
+            UserDAOJSON.getInstance().insert((User) player);
+        }
+
+        if (player instanceof Machine) {
+            MachineDAOJSON.getInstance().insert((Machine) player);
+        }
     }
 
     public static void saveGame(Game game) {
 
         if (game instanceof ComputerGame) {
-            
+
             MachineDAOJSON.getInstance().addGamePlayed();
 
             if (game.getInactivePlayer() instanceof User) {
@@ -37,7 +39,7 @@ public class DataHandler {
             if (game.getActivePlayer() instanceof User) {
                 //user perdeu
                 UserDAOJSON.getInstance().addGamePlayed(game.getActivePlayer().getUsername());
-                
+
                 MachineDAOJSON.getInstance().addVictory();
             }
         }
